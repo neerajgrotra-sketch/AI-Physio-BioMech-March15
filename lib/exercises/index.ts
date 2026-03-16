a68e7f2409efd4021bbd8b35207510fb7f158204
neerajgrotra-sketch <neerajgrotra@gmail.com>
2026-03-16 17:23:21 -0400

    Create index.ts

---
+++ lib/exercises/index.ts
@@ -0,0 +1,18 @@
+import { rightArmRaisePrescription } from "@/lib/exercises/definitions/rightArmRaise";
+import { leftArmRaisePrescription } from "@/lib/exercises/definitions/leftArmRaise";
+import { bothArmRaisePrescription } from "@/lib/exercises/definitions/bothArmRaise";
+import { sitToStandPrescription } from "@/lib/exercises/definitions/sitToStand";
+
+export const EXERCISE_PRESCRIPTIONS = [
+  rightArmRaisePrescription,
+  leftArmRaisePrescription,
+  bothArmRaisePrescription,
+  sitToStandPrescription
+];
+
+export {
+  rightArmRaisePrescription,
+  leftArmRaisePrescription,
+  bothArmRaisePrescription,
+  sitToStandPrescription
+};
