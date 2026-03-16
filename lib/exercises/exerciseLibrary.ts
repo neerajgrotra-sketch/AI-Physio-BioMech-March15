4c7dc0ecc63a5340efe6682007113ee33ec81b9d
neerajgrotra-sketch <neerajgrotra@gmail.com>
2026-03-15 20:13:14 -0400

    Create exerciseLibrary.ts

---
+++ lib/exercises/exerciseLibrary.ts
@@ -0,0 +1,33 @@
+import { ExerciseDefinition } from "@/lib/types/exercise"
+
+export const EXERCISE_LIBRARY:ExerciseDefinition[]=[
+
+{
+
+id:"right-arm-raise",
+
+name:"Right Arm Raise",
+
+description:"Lift your right arm and lower slowly.",
+
+repTarget:6,
+
+movementGoal:"Shoulder flexion"
+
+},
+
+{
+
+id:"left-arm-raise",
+
+name:"Left Arm Raise",
+
+description:"Lift your left arm and lower slowly.",
+
+repTarget:6,
+
+movementGoal:"Shoulder flexion"
+
+}
+
+]
