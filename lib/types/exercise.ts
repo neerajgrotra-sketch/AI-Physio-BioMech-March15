212cc0af852729742602fa2cc8764079ae959dcb
neerajgrotra-sketch <neerajgrotra@gmail.com>
2026-03-17 23:22:52 -0400

    Update exercise.ts

--- lib/types/exercise.ts
+++ lib/types/exercise.ts
@@ -32,6 +32,7 @@ export type MetricSource =
   | "rightWristToShoulderDy"
   | "leftWristToShoulderDy"
   | "hipCenterY"
+  | "hipHeightNormalized"
   | "hipCenterVelocityY"
   | "kneeToHipExtensionScore";
 
